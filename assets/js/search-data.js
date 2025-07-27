6b93431a697d563031db634a452ff0e5bddb47a3
prattaybhattacharya <prattay@outlook.com>
2025-07-27 23:33:14 +0000

    Deploying to gh-pages from @ prattaybhattacharya/prattaybhattacharya.github.io@0cf4c94ade40fdf064a05cc07372c971e61affdc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "STEM tutoring, SAT prep, college admissions advice, and more!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
